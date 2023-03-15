bd4bb1a8e510386c59b13d14e50b23431391bef4
rschneckenhaus <robert.schneckenhaus@flavia-it.de>
2023-03-15 14:42:51 +0100

    Add writing a whole dir + boot code option

--- Amiga.FileFormats.ADF/ADFWriter.cs
+++ Amiga.FileFormats.ADF/ADFWriter.cs
@@ -1,4 +1,6 @@
-﻿namespace Amiga.FileFormats.ADF
+﻿using System;
+
+namespace Amiga.FileFormats.ADF
 {
     public enum ADFWriteResult
     {
@@ -13,7 +15,78 @@
     // them when writing. To ease the data creation but also as it isn't the valuable in my opinion.
     public static class ADFWriter
     {
+        private static readonly byte[] DefaultBootCode = new byte[82]
+        {
+            0x43, 0xFA, 0x00, 0x3E, // lea      exp(pc),a1  ; Lib name
+            0x70, 0x25,             // moveq    #37,d0      ; Lib version
+            0x4E, 0xAE, 0xFD, 0xD8, // jsr      -552(a6)    ; OpenLibrary()
+            0x4A, 0x80,             // tst.l    d0          ; error == 0
+            0x67, 0x0C,             // beq.b    error1
+            0x22, 0x40,             // move.l   d0,a1       ; lib pointer
+            0x08, 0xE9, 0x00, 0x06, // bset     #6,34(a1)
+            0x00, 0x22,
+            0x4E, 0xAE, 0xFE, 0x62, // jsr      -414(a6)    ; CloseLibrary()
+            // error1:
+            0x43, 0xFA, 0x00, 0x18, // lea      dos(PC),a1  ; name
+            0x4E, 0xAE, 0xFF, 0xA0, // jsr      -96(a6)     ; FindResident()
+            0x4A, 0x80,             // tst.l    d0
+            0x67, 0x0A,             // beq.b    error2      ; not found
+            0x20, 0x40,             // move.l   d0,a0
+            0x20, 0x68, 0x00, 0x16, // move.l   22(a0),a0   ; DosInit sub
+            0x70, 0x00,             // moveq    #0,d0
+            0x4E, 0x75,             // rts
+            // error2:
+            0x70, 0xFF,             // moveq    #-1,d0
+            0x4E, 0x75,             // rts
+            // "dos.library"
+            0x64, 0x6F, 0x73, 0x2E,
+            0x6C, 0x69, 0x62, 0x72,
+            0x61, 0x72, 0x79, 0x00,
+            // "expansion.library"
+            0x65, 0x78, 0x70, 0x61,
+            0x6E, 0x73, 0x69, 0x6F,
+            0x6E, 0x2E, 0x6C, 0x69,
+            0x62, 0x72, 0x61, 0x72,
+            0x79, 0x00
+        };
+
         internal delegate void WriteSectors(int index, int count, Action<DataWriter> writeHandler);
+
+        public static ADFWriteResult WriteADFFile(string adfFilePath, string name, string directoryPath,
+            bool includeEmptyDirectories, string? fileFilter, FileSystem fileSystem,
+            bool bootable, bool internationalMode, bool hd)
+        {
+            var files = Directory.GetFiles(directoryPath, fileFilter ?? "*", SearchOption.AllDirectories);
+            var directories = includeEmptyDirectories
+                ? Directory.GetDirectories(directoryPath, "*", SearchOption.AllDirectories)
+                    .Where(d => !files.Any(f => f.Contains(d, StringComparison.InvariantCultureIgnoreCase)))
+                    .ToArray()
+                : null;
+
+            var config = new ADFWriterConfiguration
+            {
+                FileSystem = fileSystem,
+                InternationalMode = internationalMode,
+                HD = hd,
+                DiskFullBehavior = DiskFullBehavior.Error,
+                BootCode = bootable ? DefaultBootCode : null
+            };
+
+            using var stream = File.Create(adfFilePath);
+            int rootLength = directoryPath.Length;
+
+            if (!directoryPath.EndsWith('/') && !directoryPath.EndsWith('\\'))
+                ++rootLength;
+
+            string GetRelativePath(string path)
+            {
+                return path[rootLength..].Replace('\\', '/').TrimEnd('/');
+            }
+
+            return WriteADFFile(stream, name, config,
+                files.ToDictionary(f => GetRelativePath(f), f => File.ReadAllBytes(f)),
+                directories?.Select(d => GetRelativePath(d))?.ToList());
+        }
 
         public static ADFWriteResult WriteADFFile(Stream stream, string name, ADFWriterConfiguration configuration,
             Dictionary<string, Stream> files, List<string>? emptyDirectoryPaths = null)
