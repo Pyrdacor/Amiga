5054aaaeebf6b25f8e6da258b1885461c0697459
Pyrdacor <trobt@web.de>
2024-08-13 17:12:13 +0200

    Added LHA nuget package

--- nuget.ps1
+++ nuget.ps1
@@ -4,4 +4,5 @@ if ($isWindows) {
   Write-Host Copy nuget packages
   mkdir "nuget"
   xcopy /Y /I "Amiga.FileFormats.ADF\bin\Release\*.nupkg" "nuget\*"
+  xcopy /Y /I "Amiga.FileFormats.LHA\bin\Release\*.nupkg" "nuget\*"
 }
