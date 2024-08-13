03fde521b7d3580b4279964c061a8f1a2e2d976e
Pyrdacor <trobt@web.de>
2024-08-13 17:06:48 +0200

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -5,8 +5,8 @@ if ($isWindows) {
   dotnet publish -c Release ./AmigaAdf/AmigaAdf.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r win-x64 --no-restore --nologo --self-contained
   dotnet publish -c Release ./AmigaAdf/AmigaAdf.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r win-x86 --no-restore --nologo --self-contained
   Write-Host Pack zips for Windows
-  7z a AmigaTools-Windows.zip "./AmigaAdf/bin/Any CPU/Release/net7.0/win-x64/publish/AmigaAdf.exe" "./x64/api-ms-win-core-winrt-l1-1-0.dll" -mx9
-  7z a AmigaTools-Windows.zip "./AmigaAdf/bin/Any CPU/Release/net7.0/win-x86/publish/AmigaAdf.exe" "./x86/api-ms-win-core-winrt-l1-1-0.dll" -mx9
+  7z a AmigaTools-Windows-x64.zip "./AmigaAdf/bin/Any CPU/Release/net7.0/win-x64/publish/AmigaAdf.exe" "./x64/api-ms-win-core-winrt-l1-1-0.dll" -mx9
+  7z a AmigaTools-Windows-x86.zip "./AmigaAdf/bin/Any CPU/Release/net7.0/win-x86/publish/AmigaAdf.exe" "./x86/api-ms-win-core-winrt-l1-1-0.dll" -mx9
 } elseif ($isLinux) {
   Write-Host Publish Linux executable
   dotnet publish -c Release ./AmigaAdf/AmigaAdf.csproj -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -r linux-x64 --no-restore --nologo --self-contained
@@ -21,7 +21,7 @@ if ($isWindows) {
   Write-Host Pack zips for Mac
   sudo xcode-select -s /Applications/Xcode-13.2.1.app
   Start-Process -FilePath codesign -Wait -WorkingDirectory . -ArgumentList '-s','-','--force','--verbose','--deep','--no-strict','"./AmigaAdf/bin/Any CPU/Release/net7.0/osx-x64/publish/AmigaAdf"'
-  7z a AmigaAdf-Mac.zip "./AmigaAdf/bin/Any CPU/Release/net7.0/osx-x64/publish/AmigaAdf" -mx9
+  7z a AmigaTools-Mac-Intel.zip "./AmigaAdf/bin/Any CPU/Release/net7.0/osx-x64/publish/AmigaAdf" -mx9
   Start-Process -FilePath codesign -Wait -WorkingDirectory . -ArgumentList '-s','-','--force','--verbose','--deep','--no-strict','"./AmigaAdf/bin/Any CPU/Release/net7.0/osx-arm64/publish/AmigaAdf"'
-  7z a AmigaAdf-Mac.zip "./AmigaAdf/bin/Any CPU/Release/net7.0/osx-arm64/publish/AmigaAdf" -mx9
+  7z a AmigaTools-Mac-Arm.zip "./AmigaAdf/bin/Any CPU/Release/net7.0/osx-arm64/publish/AmigaAdf" -mx9
 }
